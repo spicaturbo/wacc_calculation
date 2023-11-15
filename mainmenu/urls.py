ccf469b62eacdfb9415f8a63815595ff2701d6c1
alvin <alvinecom2018@gmail.com>
2023-11-15 18:24:18 +0700

    fixing

--- mainmenu/urls.py
+++ mainmenu/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.my_view, name="my_view"),
-    path("wacc_calculation/", views.wacc_view, name="wacc_view")
+    path("wacc_calculation/", views.wacc_view, name="wacc_view"),
 ]
--- mainmenu/views.py
+++ mainmenu/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import SumForm
 
 # Create your views here.
 def my_view(request):
@@ -74,11 +72,16 @@ def wacc_view(request):
         num1 = float(request.POST.get('num1', 0))
         num2 = float(request.POST.get('num2', 0))
         num3 = float(request.POST.get('num3', 0))
+        num3 = num3 / 100
         num4 = float(request.POST.get('num4', 0))
+        num4 = num4 / 100
         num5 = float(request.POST.get('num5', 0))
+        num5 = num5 / 100
         num6 = float(request.POST.get('num6', 0))
         num7 = float(request.POST.get('num7', 0))
+        num7 = num7 / 100
         num8 = float(request.POST.get('num8', 0))
+        num8 = num8 / 100
         EBIT = float(request.POST.get('EBIT', 0))
         DA = float(request.POST.get('DA', 0))
         CapEx = float(request.POST.get('CapEx', 0))
@@ -90,16 +93,16 @@ def wacc_view(request):
         OSH = float(request.POST.get('OSH', 0))
 
         # Result
-        COE = num5 + num6 * (num7 - num5)
-        EED = num1 / (num1 + num2)
-        DED = num2 / (num1 + num2)
-        WACC = (EED * COE) + (DED * num3 * (1 - num4))
+        COE = (num5 + num6 * (num7 - num5))
+        EED = (num1 / (num1 + num2))
+        DED = (num2 / (num1 + num2))
+        WACC = ((EED * COE) + (DED * num3 * (1 - num4)))
 
         # Result decimal
-        result1 = f"{COE:.3f}"
-        result2 = f"{EED:.3f}"
-        result3 = f"{DED:.3f}"
-        result4 = f"{WACC:.3f}"
+        result1 = f"{COE * 100:.3f}"
+        result2 = f"{EED * 100 :.3f}"
+        result3 = f"{DED * 100:.3f}"
+        result4 = f"{WACC * 100:.3f}"
 
         arrayYear = [i for i in range(0, year + 1)]
 
@@ -107,42 +110,42 @@ def wacc_view(request):
         for index, yr in enumerate(arrayYear):
             if index == 0:
                 dec1 = EBIT * ((1 + num8) ** yr )
-                rEBIT = f"{dec1:.0f}"
+                rEBIT = f"{dec1:.3f}"
                 rTax = EBIT * -num4
             else :
                 dec1 = float(arrayEBIT[index - 1]) * ((1 + num8) ** yr )
-                rEBIT = f"{dec1:.0f}"
+                rEBIT = f"{dec1:.3f}"
                 dec2 = float(dec1) * -num4
-                rTax = f"{dec2:.0f}"
+                rTax = f"{dec2:.3f}"
 
             if index == 0:
                 dec1 = DA * ((1 + num8) ** yr )
-                rDA = f"{dec1:.0f}"
+                rDA = f"{dec1:.3f}"
             else :
                 dec1 = float(arrayDA[index - 1]) * ((1 + num8) ** yr )
-                rDA = f"{dec1:.0f}"
+                rDA = f"{dec1:.3f}"
 
             if index == 0:
                 dec1 = CapEx * ((1 + num8) ** yr )
-                rCapex = f"{dec1:.0f}"
+                rCapex = f"{dec1:.3f}"
             else :
                 dec1 = float(arrayCapEx[index - 1]) * ((1 + num8) ** yr )
-                rCapex = f"{dec1:.0f}"
+                rCapex = f"{dec1:.3f}"
 
             if index == 0:
                 dec1 = NonCashWC * ((1 + num8) ** yr )
-                rNoncashwc = f"{dec1:.0f}"
+                rNoncashwc = f"{dec1:.3f}"
             else :
                 dec1 = float(arrayNonCashWC[index - 1]) * ((1 + num8) ** yr )
-                rNoncashwc = f"{dec1:.0f}"
+                rNoncashwc = f"{dec1:.3f}"
 
             rFCF = float(rEBIT) + float(rDA) + float(rTax) + float(rCapex) + float(rNoncashwc)
 
             if index != 0:
-                dec1 = 1 / ((1 + WACC) ** yr)
+                dec1 = (1 / ((1 + WACC) ** yr))
                 rDiscount = f"{dec1:.2f}"
-                dec2 = float(rDiscount) * float(rFCF)
-                rPVFCF = f"{dec2:.0f}"
+                dec2 = (float(rDiscount) * float(rFCF))
+                rPVFCF = f"{dec2:.3f}"
 
             arrayEBIT.append(rEBIT)
             arrayTax.append(rTax)
@@ -154,10 +157,10 @@ def wacc_view(request):
             arrayPVFCF.append(rPVFCF)
 
         TV = (float(arrayFCF[-1]) * (1 + num8)) / (WACC - num8)
-        resultTV = f"{TV:.0f}"
+        resultTV = f"{TV:.3f}"
 
         PVTV = float(arrayDiscount[-1]) * float(resultTV)
-        rPVTV = f"{PVTV:.0f}"
+        rPVTV = f"{PVTV:.3f}"
 
         res = 0
 
@@ -167,10 +170,10 @@ def wacc_view(request):
 
         res = res + PVTV
 
-        EnterpriseValue = f"{res:.0f}"
+        EnterpriseValue = f"{res:.3f}"
 
         result = float(EnterpriseValue) + Cash + MS - STD - LTD
-        EquityValue = f"{result:.0f}"
+        EquityValue = f"{result:.3f}"
 
         results = float(EquityValue)/OSH
         IntrinsicValue = f"{results:.2f}"
